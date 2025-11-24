614eafb57975c523828d6b312dde043e20214132
atanael mendes <programadormendes@gmail.com>
2025-11-24 14:03:04 -0300

    removendo lixo

--- app/components/cliente/CadastroCliente.jsx
+++
@@ -1,7 +0,0 @@
-import Iframe from "../Layouts/Iframe";
-const CadastroCliente = ({cdPessoaCliente, cdEmpresa, className}) => {
-    return (
-        <Iframe active={true} visible={true} className={className || ''} src="rhbsaas/ger_v2/prospects_edit.php" params={{cd: cdPessoaCliente, cd_unid: cdEmpresa, pro:2}} />
-    );
-}
-export default CadastroCliente;
